974648e1fba506df0b5eb7377a3d2bf84d4d711f
Lautaro <lautaroaguilera588@gmail.com>
2023-07-04 19:44:36 -0300

    Add library post method endpoint and the express json built-in middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3000;
 
 const { initializeDB } = require('./config/db-config');
+const { Library } = require('./models');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.end();
@@ -10,6 +13,16 @@ app.get('/', (req, res) => {
 
 app.get('/library', (req, res) => {
   res.end();
+});
+
+app.post('/library', async (req, res) => {
+  try {
+    const newLibrary = await Library.create(req.body);
+    res.status(201).json(newLibrary);
+  } catch (err) {
+    console.error("Error when creating library", err);
+    res.status(500).json({ action: "createLibrary", error: err.message });
+  }
 });
 
 app.listen(port, async () => {
