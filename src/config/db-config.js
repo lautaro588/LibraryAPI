27c2a8f8ed50c1a9ac6464b12d2e1f02957302fd
Lautaro <lautaroaguilera588@gmail.com>
2023-07-05 16:24:58 -0300

    Disable Sequelize logging

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -3,6 +3,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "./database.sqlite",
+  logging: false
 });
 
 const initializeDB = async () => {
