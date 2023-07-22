6b7cb3b385d8381a8bbbcab4bba0ba246e40af9c
Lautaro <lautaroaguilera588@gmail.com>
2023-07-22 17:06:39 -0300

    Add user model

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,8 @@
 const Library = require("./library");
 const Book = require("./book");
+const User = require("./user")
 
 Library.hasMany(Book, { as: 'books', foreignKey: 'library' });
 Book.belongsTo(Library, { foreignKey: 'library' });
 
-module.exports = { Library, Book };
+module.exports = { Library, Book, User };
