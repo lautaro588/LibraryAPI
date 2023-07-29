01fdea2f3a6978dd56871d662fc5b50b41595381
Lautaro <lautaroaguilera588@gmail.com>
2023-07-28 21:18:47 -0300

    Change login response from the validated user to a jwt

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,7 +4,11 @@ const login = async (req, res) => {
     try {
         const { username, password } = req.body;
         const validUser = await userService.validateUser(username, password);
-        validUser ? res.json(validUser) : res.status(401).json({ error: 'Invalid User' });
+        if (validUser) {
+            const token = userService.getUserToken(username);
+            return res.json(token);
+        };
+        res.status(401).json({ error: 'Invalid User' });
     } catch (err) {
         res.status(500).json({ action: 'Login', error: err.message });
     }
